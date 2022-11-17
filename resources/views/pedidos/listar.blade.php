a42b2de73ffd22187ea4c493f88dd676d216ca28
tiago-rodrigues1 <tiagords127@gmail.com>
2022-11-17 16:02:51 -0300

    feat: implementa navmenu

--- resources/views/pedidos/listar.blade.php
+++ resources/views/pedidos/listar.blade.php
@@ -12,46 +12,7 @@
 </head>
 
 <body>
-    <nav class="tc-nav navbar navbar-dark navbar-expand-lg bg-tc-green align-items-center px-2 mb-">
-        <div class="container-fluid">
-            <h1 class="navbar-brand fs-3 text-tc-white">Trairi Commerce</h1>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos/Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sua conta</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Notificações
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pedidos</a>
-                    </li>
-                </ul>
-                <form class="d-flex col-12 col-lg-5 mb-2" role="search">
-                    <input class="form-control me-2 bg-tc-white" type="search" placeholder="O que você procura ?"
-                        aria-label="Search">
-                    <button class="btn bg-tc-green-dark px-4 border-0" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="#F7F7F7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-search">
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                        </svg>
-                    </button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <x-navmenu />
 
     <main class="d-flex flex-column flex-lg-row max-w-100">
         <!-- <filters> -->
