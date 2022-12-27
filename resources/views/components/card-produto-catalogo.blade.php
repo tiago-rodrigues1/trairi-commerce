f50addee5f69f1a4a373006778d1b049e59dac64
tiago-rodrigues1 <tiagords127@gmail.com>
2022-12-27 16:05:47 -0300

    add CardProdutoCatalogo component

---
+++ resources/views/components/card-produto-catalogo.blade.php
@@ -0,0 +1,22 @@
+<article class="card p-0 mx-2 my-2">
+    <img src="/images/pizza.jpg" alt="Pizza" class="card-img-top">
+
+    <main class="p-3 bg-tc-gray vstack gap-3 card-body">
+        <section class="vstack gap-3">
+            <h4>Pizza de calabresa</h4>
+            <div class="hstack align-items-center justify-content-between">
+                <h5 class="fw-normal m-0 p-0">R$ 40,00</h5>
+                <span>Disponível</span>
+            </div>
+        </section>
+        <div class="vstack gap-2 py-1">
+            <span>Categoria: Alimentos</span>
+            <span>Adicionado em 23/12/2022</span>
+        </div>
+        <hr class="my-1">
+        <section class="hstack align-items-center justify-content-between">
+            <button class="btn tc-btn">Editar</button>
+            <button class="btn tc-btn-outline-red">Excluir</button>
+        </section>
+    </main>
+</article>
