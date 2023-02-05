3c6d66bab240dc04e95cea72a8a0b19a5cfbd742
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-04 23:00:09 -0300

    implmenta componente de seção de produtos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,30 +3,28 @@
 @section('title', 'Trairi Commerce')
 
 @section('content')
-    <main class="p-5 vstack gap-5">
-        <section class="col-12 col-lg-9 col-xl-10">
-            <h1 class="fs-3 pb-4">Produtos mais vendidos</h1>
-            <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 gap-lg-4">
-                <x-cardproduto />
-                <x-cardproduto />
-                <x-cardproduto />
-            </div>
-        </section>
-        <section class="col-12 col-lg-9 col-xl-10">
-            <h1 class="fs-3 pb-4">Serviços mais solicitados</h1>
-            <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 gap-lg-4">
-                <x-cardproduto />
-                <x-cardproduto />
-                <x-cardproduto />
-            </div>
-        </section>
-        <section class="col-12 col-lg-9 col-xl-10">
-            <h1 class="fs-3 pb-4">Produtos mais vendidos</h1>
-            <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 gap-lg-4">
-                <x-cardproduto />
-                <x-cardproduto />
-                <x-cardproduto />
-            </div>
-        </section>
+    <main class="w-100 d-flex flex-column align-items-center p-5 gap-5">
+        <x-product-section sectionTitle="Mais estrelas">
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+        </x-product-section>
+        <x-product-section sectionTitle="Serviços mais solicitados">
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+        </x-product-section>
+        <x-product-section sectionTitle="Mais vendidos">
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+            <x-cardproduto />
+        </x-product-section>
     </main>
 @endsection
