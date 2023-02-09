56e3e48806f9129cc1a5beed8dc101bc56fa4493
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-09 16:32:11 -0300

    refactor Produto model

--- app/Models/ProdutoImagem.php
+++ app/Models/ProdutoImagem.php
@@ -9,19 +9,18 @@ class ProdutoImagem extends Model {
     use HasFactory;
 
     protected $fillable = ['path'];
+    protected $table = "produtos_imagens";
 
     public function produto() {
         return $this->belongsTo(Produto::class);
     }
 
-    public function salvar($imgs, $produto) {
+    public static function salvar($imgs, $produto) {
         foreach ($imgs as $img) {
             $path = $img->store('imagens', 'public');
 
             $pi = new ProdutoImagem(['path' => $path]);
             $produto->imagens()->save($pi);
-
-            $pi->save();
         }
     }
 }
