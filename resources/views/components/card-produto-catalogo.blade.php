571fc5f6c45f5d367b99efaaf576dc5485551d27
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-10 11:55:07 -0300

    card produto catalogo com dados dinamicos

--- resources/views/components/card-produto-catalogo.blade.php
+++ resources/views/components/card-produto-catalogo.blade.php
@@ -1,17 +1,19 @@
 <article class="card p-0 mx-2 my-2">
-    <img src="/images/pizza.jpg" alt="Pizza" class="card-img-top">
+    <img src="/storage/{{ $produto->imagens[0]->path }}" alt="Pizza" class="card-img-top">
 
     <main class="p-3 bg-tc-gray vstack gap-3 card-body">
         <section class="vstack gap-3">
-            <h4>Pizza de calabresa</h4>
+            <h4>{{ $produto->nome }}</h4>
             <div class="hstack align-items-center justify-content-between">
-                <h5 class="fw-normal m-0 p-0">R$ 40,00</h5>
-                <span>Disponível</span>
+                <h5 class="fw-normal m-0 p-0">R$ {{ number_format($produto->valor, 2, ',') }}</h5>
+                <span>
+                    {{ $produto->disponibilidade ? "Disponível" : "Indisponível" }}
+                </span>
             </div>
         </section>
         <div class="vstack gap-2 py-1">
-            <span>Categoria: Alimentos</span>
-            <span>Adicionado em 23/12/2022</span>
+            <span>Categoria: {{ $produto->categoria->nome }}</span>
+            <span>Adicionado em {{ date('d/m/Y',strtotime($produto->created_at)) }}</span>
         </div>
         <hr class="my-1">
         <section class="hstack align-items-center justify-content-between">
