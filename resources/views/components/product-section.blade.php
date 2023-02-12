8d5837800b2eb535b881f2422ff959d9f2c1cce1
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-11 22:00:21 -0300

    ajusta alinhamento

--- resources/views/components/product-section.blade.php
+++ resources/views/components/product-section.blade.php
@@ -1,9 +1,9 @@
-<section class="col-12 col-lg-9 col-xl-10">
+<section class="col-12 col-lg-9 col-xl-10 justify-content-center">
     @if ($sectionTitle != null)
         <h1 class="fs-3 pb-4">{{ $sectionTitle }}</h1>
     @endif
     
-    <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4 justify-content-center justify-content-md-start">
         {{ $slot }}
     </div>
 </section>
