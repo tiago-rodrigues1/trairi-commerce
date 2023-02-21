7c1ccc5ce22c896b602af437d798e7058e49a942
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-21 11:29:27 -0300

    refatora tela de pedidos

--- resources/views/pedidos/novo.blade.php
+++ resources/views/pedidos/novo.blade.php
@@ -4,37 +4,74 @@
 
 @section('content')
     <form class="vstack py-md-5 align-items-center justify-content-center rounded-3">
-        <div class="bg-tc-gray h-100 h-md-auto col-12 col-md-10 col-xl-6 p-4 rounded-3">
+        <div class="bg-tc-gray h-100 h-md-auto col-12 col-md-10 col-xl-7 p-4 rounded-3">
             <header class="vstack gap-3 pb-3">
                 <h1 class="fs-2 text-tc-green">Seu pedido</h1>
-                <div>
-                    <h3 class="fs-4 pb-1">Pizza de calabresa no capricho</h3>
-                    <h6 class="fw-normal">Por <span>Pizzaria Trairi</span></h6>
-                </div>
             </header>
             <main>
-                <section class="border-bottom">
-                    <p>Valor: <b>R$ 40,00</b></p>
-                    <p>Frete: <b>R$ 10,00</b></p>
-                    <div class="col-4 pb-3">
-                        <label class="form-label" for="quantidade">Quantidade</label>
-                        <input class="form-control" type="number" value="1" name="quantidade">
-                    </div>
-                    <div class="col-4 pb-3">
-                        <label class="form-label" for="modo-pagamento">Modo de pagamento</label>
-                        <select class="form-select" aria-label="Selecionar modo de pagamento">
-                            <option selected>Selecione uma opção</option>
-                            <option value="1">Dinheiro</option>
-                            <option value="2">Pix</option>
-                            <option value="3">Cartão</option>
-                        </select>
-                    </div>
-                    <div class="col-8 pb-3">
-                        <label class="form-label" for="observacoes">Deixe um comentário</label>
-                        <textarea class="form-control" name="observacoes" id="observacoes" rows="5"></textarea>
-                    </div>
-
-                    <p class="fs-5"><b>Total: </b> <span>R$ 50,00</span></p>
+                <div class="table-responsive mt-3">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Título</th>
+                                <th scope="col">Preço</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Ação</th>
+                            </tr>
+                        </thead>
+                        @if (session()->has('usuario.carrinho'))
+                        <tbody>
+                            @php
+                                $subtotal = 0;
+                            @endphp
+                            @foreach (session()->get('usuario.carrinho') as $index=>$produto)
+                                @php
+                                    $subtotal += $produto->valor;
+                                @endphp
+                                <tr>
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $produto->nome }}</td>
+                                    <td>{{ number_format($produto->valor, 2, ',') }}</td>
+                                    <td>
+                                        <input type="number" class="form-control quant_item" style="width: 5rem;"
+                                            name="quantidade" value="1" data-price="{{ $produto->valor }}">
+                                    </td>
+                                    <td>
+                                        <button class="btn bg-danger text-white">Excluir</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td>#</td>
+                                <td><strong>Subotal</strong></td>
+                                <td id="subtotal">{{ number_format($subtotal, 2, ',') }}</td>
+                                <td><strong>Frete</strong></td>
+                                <td>R$ 3,00</td>
+                            </tr>
+                            <tr>
+                                <td colspan="5" class="text-center text-tc-green fw-bold" style="font-size: 1.125rem">
+                                    Total: R$ 10,00</td>
+                            </tr>
+                        </tfoot>
+                        @endif
+                    </table>
+                </div>
+                <div class="col-4 my-3">
+                    <label class="form-label" for="modo-pagamento">Modo de pagamento</label>
+                    <select class="form-select" aria-label="Selecionar modo de pagamento">
+                        <option selected>Selecione uma opção</option>
+                        <option value="1">Dinheiro</option>
+                        <option value="2">Pix</option>
+                        <option value="3">Cartão</option>
+                    </select>
+                </div>
+                <div class="col-8 pb-3">
+                    <label class="form-label" for="observacoes">Deixe um comentário</label>
+                    <textarea class="form-control" name="observacoes" id="observacoes" rows="5"></textarea>
+                </div>
                 </section>
                 <section class="pt-3">
                     <div class="hstack gap-3 align-items-center">
@@ -55,10 +92,8 @@
                             <b>Endereço: </b>Rua dos bobos, Nº 0
                         </li>
                     </ul>
-                    <div class="vstack gap-3 py-3">
-                        <button class="btn tc-btn">Enviar pedido</button>
-                        <button class="btn tc-btn-outline-red">Cancelar</button>
-                    </div>
+                    <button class="btn tc-btn w-100 my-3" type="submit" {{ session()->has('usuario.carrinho') ? "" : "disabled" }}>Enviar</button>
+                    <a class="btn tc-btn-ghost-green w-100" href="/">Continuar comprando</button>
                 </section>
             </main>
         </div>
