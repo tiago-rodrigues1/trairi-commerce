183dcdefe4cd0fe84048b1ec78e3d34083c8ea68
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-21 11:31:45 -0300

    produtos vindos do db

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,10 @@
 
 @section('content')
     <main class="w-100 d-flex flex-column align-items-center p-5 gap-5">
-        <x-product-section sectionTitle="Mais estrelas">
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-        </x-product-section>
-        <x-product-section sectionTitle="Serviços mais solicitados">
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-        </x-product-section>
-        <x-product-section sectionTitle="Mais vendidos">
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
-            <x-cardproduto />
+        <x-product-section sectionTitle="Mais recentes">
+            @foreach ($produtos as $produto)
+                <x-card-produto :produto="$produto" />
+            @endforeach
         </x-product-section>
     </main>
 @endsection
