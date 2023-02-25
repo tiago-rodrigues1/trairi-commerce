04687fa5d517449f2157464d9bd3e5284c9b51d2
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-25 14:10:34 -0300

    refactor: add atributos

--- database/migrations/2022_11_01_190858_create_pedidos_table.php
+++ database/migrations/2022_11_01_190858_create_pedidos_table.php
@@ -4,19 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->string('estado', 45);
             $table->dateTime('data_hora');
+            $table->string('cep_destino', 100);
+            $table->string('cidade_destino', 100);
+            $table->string('bairro_destino', 100);
+            $table->string('endereco_destino', 100);
+            $table->string('observacao', 500);
             $table->foreignId('tipo_de_pagamento_id')->constrained();
             $table->foreignId('cliente_id')->constrained();
             $table->timestamps();
