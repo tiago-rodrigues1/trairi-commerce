0018c99bfe29783a165a6fde30536743e1eef050
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-27 21:15:43 -0300

    add remover item do pedido

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,17 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pedido;
 use App\Models\Produto;
+use Illuminate\Http\Request;
 
 class PedidoController extends Controller {
     public function renderPedidoNovo() {
-        return view('pedidos/novo');
+        $u = session()->get('usuario');
+        return view('pedidos/novo', compact('u'));
     }
 
     public function adicionarItem($id) {
         $produto = Produto::find($id);
-        session()->push('usuario.carrinho', $produto);
+        session()->push('carrinho', $produto);
 
-        return redirect('/pedidos/novo');
+        return redirect('/pedidos/items');
+    }
+
+    public function removerItem($id) {
+        $produtos = session()->pull('carrinho', []);
+
+        define('ID_TO_DELETE', $id);
+        
+        array_walk($produtos, function($produto, $key) use ($produtos) {
+            if ($produto->id == ID_TO_DELETE) {
+                unset($produtos[$key]);
+                session()->put('carrinho', $produtos);
+            }
+        });
+        
+        return redirect('/pedidos/items');
+    }
+
+    public function novoPedido(Request $request) {
+        $pedido = new Pedido();
     }
 }
