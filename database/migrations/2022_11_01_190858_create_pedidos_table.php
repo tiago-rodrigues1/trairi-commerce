2a7c40b33f85ce0178e6fa02e33206a4c9586ddd
tiago-rodrigues1 <tiagords127@gmail.com>
2023-02-28 11:55:02 -0300

    refatora tabelas pedidos e contems

--- database/migrations/2022_11_01_190858_create_pedidos_table.php
+++ database/migrations/2022_11_01_190858_create_pedidos_table.php
@@ -13,15 +13,14 @@ return new class extends Migration {
     public function up() {
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
-            $table->string('estado', 45);
-            $table->dateTime('data_hora');
-            $table->string('cep_destino', 100);
-            $table->string('cidade_destino', 100);
-            $table->string('bairro_destino', 100);
-            $table->string('endereco_destino', 100);
-            $table->string('observacao', 500);
             $table->foreignId('tipo_de_pagamento_id')->constrained();
             $table->foreignId('cliente_id')->constrained();
+            $table->string('estado', 45);
+            $table->string('cep_destino', 9);
+            $table->string('cidade_destino', 100);
+            $table->string('bairro_destino', 100);
+            $table->string('endereco_destino', 300);
+            $table->string('observacao', 500)->nullable();
             $table->timestamps();
         });
     }
