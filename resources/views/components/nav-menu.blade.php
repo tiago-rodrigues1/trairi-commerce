1505574eba6e727e2457500ec3af8f891a125dfa
tiago-rodrigues1 <tiagords127@gmail.com>
2023-03-02 11:22:49 -0300

    add link de listagem de pedidos

--- resources/views/components/nav-menu.blade.php
+++ resources/views/components/nav-menu.blade.php
@@ -42,7 +42,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pedidos</a>
+                        <a class="nav-link" href="/pedidos/listar">Pedidos</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/usuario/deslogar">Sair</a>
