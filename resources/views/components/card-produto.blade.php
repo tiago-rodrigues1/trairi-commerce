6674afb8bd1ae0ee26828edc610468b6d66e9c35
tiago-rodrigues1 <tiagords127@gmail.com>
2023-03-06 22:28:02 -0300

    refatora estilos de card-produto e detalhes

--- resources/views/components/card-produto.blade.php
+++ resources/views/components/card-produto.blade.php
@@ -24,26 +24,27 @@
             </section>
             <hr class="my-1">
             <section class="vstack gap-1">
-                <h6>Aceitamos</h6>               
+                <h6>Aceitamos</h6>
                 <div class="w-100 hstack align-items-center justify-content-evenly">
                     @foreach ($produto->anunciante->tiposDePagamento as $tipo_pagamento)
-                        @if ($tipo_pagamento-> id == 1)
-                        <span
-                            class="d-flex align-items-center justify-content-center badge bg-success bg-opacity-50 p-2 rounded-2"> 
-                            {{ $tipo_pagamento->descricao}}
-                        </span>
-                        @elseif ($tipo_pagamento-> id == 2)
+                        @if ($tipo_pagamento->id == 1)
+                            <span
+                                class="d-flex align-items-center justify-content-center badge bg-success bg-opacity-50 p-2 rounded-2">
+                                {{ $tipo_pagamento->descricao }}
+                            </span>
+                        @elseif ($tipo_pagamento->id == 2)
                             <span
                                 class="d-flex align-items-center justify-content-center badge bg-danger bg-opacity-50 p-2 rounded-2">
-                                {{ $tipo_pagamento->descricao}}
+                                {{ $tipo_pagamento->descricao }}
                             </span>
-                        @elseif ($tipo_pagamento-> id == 3 && 4)
+                        @elseif ($tipo_pagamento->id == 3 || $tipo_pagamento->id == 4)
                             <span
                                 class="d-flex align-items-center justify-content-center badge bg-primary bg-opacity-50 p-2 rounded-2">
-                                {{ $tipo_pagamento->descricao}}
-                            </span>                          
-                            @endif
-                    @endforeach                                               
+                                Cartão
+                            </span>
+                            @break
+                        @endif
+                    @endforeach
                 </div>
             </section>
         </main>
