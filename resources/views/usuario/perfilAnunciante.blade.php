3ca08cf0cbf51994cf0a81accdef9925d2b51c4a
Natalya <nunes.natalya23@gmail.com>
2023-06-25 20:10:26 -0300

    add icons e loc

--- resources/views/usuario/perfilAnunciante.blade.php
+++ resources/views/usuario/perfilAnunciante.blade.php
@@ -15,35 +15,35 @@
                                 <h3><b>{{ $anunciante->nome_fantasia }}</b></h3>
                             </div>
                             <p>{{ $anunciante->descricao }}</p>
-                            <span>Localização: Avenida 25 de março</span>
+                            <span>Localização: {{ $anunciante->cidade }}, {{ $anunciante->bairro }} Rua {{ $anunciante->endereco }}</span>
                         </div>
                     </div>
                     <div class="vstack bg-tc-gray border rounded-3  align-items-center gap-2 p-2">
                         <span><b>Canais de atendimento</b> </span>
                         <div class="gap-4 p-2 hstack justify-content-center">
                             <div class=" ">
-                                <img src="/images/perfil.jpg" alt="" class="rounded-circle"
+                                <img src="/icons/canais_anunciante/email-svgrepo-com.svg" alt="" class="rounded-circle"
                                     style="width: 2.5rem; height: 2.5rem;">
-                                    <span>cleitin_junior23</span>
+                                    <span>{{ $anunciante->email_anunciante }}</span>
                             </div>
 
                             <div class="  ">
-                                <img src="/images/perfil.jpg" alt="" class="rounded-circle"
+                                <img src="/icons/canais_anunciante/whatsapp-svgrepo-com.svg" alt="" class="rounded-circle"
                                     style="width: 2.5rem; height: 2.5rem;">
-                                    <span>cleitin_junior23</span>
+                                    <span data-mask="(00) 00000-0000">{{ $anunciante->whatsapp }}</span>
                             </div>
                         </div>
                         <div class="gap-4 p-2 hstack align-items-center justify-content-center">
                             <div class=" ">
-                                <img src="/images/perfil.jpg" alt="" class="rounded-circle"
+                                <img src="/icons/canais_anunciante/instagram-svgrepo-com.svg" alt="" class="rounded-circle"
                                     style="width: 2.5rem; height: 2.5rem;">
-                                    <span>cleitin_junior23</span>
+                                    <span>{{ $anunciante->instagram }}</span>
                             </div>
 
                             <div class=" ">
-                                <img src="/images/perfil.jpg" alt="" class="rounded-circle"
+                                <img src="/icons/canais_anunciante/facebook-boxed-svgrepo-com.svg" alt="" class="rounded-circle"
                                     style="width: 2.5rem; height: 2.5rem;">
-                                    <span>cleitin_junior23</span>
+                                    <span>{{ $anunciante->facebook }}</span>
                             </div>
                         </div>
 
