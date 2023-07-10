bad50ddbaaac04cf21c0fff4b546c31630cd9096
Jamilly Campelo <jamillyemilly.je@gmail.com>
2023-07-10 17:31:15 -0300

    cria migrations de comentários

---
+++ database/migrations/2023_07_10_160924_cliente_comenta_anunciantes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente_comenta_anunciantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('comentario', 300);
+            $table->foreignId('cliente_id')->constrained();
+            $table->foreignId('anunciante_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente_comenta_anunciantes');
+    }
+};
